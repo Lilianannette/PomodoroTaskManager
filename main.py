5f6e7a75594dcfc30ee1fbaafcdadcec419e4da4
lilian annette <lilianannettepro@gmail.com>
2025-01-08 17:45:02 +0100

    done

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-# from tasks import displayTask, addTask, recoverTask
-# from pomodoro import startPomodoro
+from tasks import displayTask, addTasks, deleteTask
+from pomodoro import PomodoroManager
+from gui import PomodoroGUI
 
 def menu():
+    manager = PomodoroManager()
+
     while True:
         print("\nMenu :")
         print("1. Afficher les tâches")
         print("2. Ajouter une tâche")
         print("3. Supprimer une tâche")
-        print("4. Démarrer une session pomodoro")
+        print("4. Démarrer une session Pomodoro")
         print("5. Quitter")
 
-        choice = int(input("Choissisez une option: \n "))
+        choice = int(input("Choisissez une option : "))
         if choice == 1:
             displayTask()
         elif choice == 2:
-            addTask()
+            addTasks()
         elif choice == 3:
             deleteTask()
         elif choice == 4:
-            startPomodoro()
+            manager.start_pomodoro()
         elif choice == 5:
-            print("A bientôt !")
+            print("À bientôt !")
             break
         else:
-            print("Option Invalide, Essayez encore")
+            print("Option invalide, essayez encore.")
 
 if __name__ == "__main__":
-    menu()
+    print("Choisissez votre mode d'utilisation :")
+    print("1. Mode texte")
+    print("2. Interface graphique")
+
+    mode = input("Entrez le numéro du mode : ")
+    if mode == "1":
+        menu()
+    elif mode == "2":
+        PomodoroGUI()
+    else:
+        print("Option invalide.")
